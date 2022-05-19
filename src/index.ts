fd911847207b3fd2b6250464cbf0c403cf8aa482
cutejh118 <cooljh118@gmail.com>
2022-05-19 09:52:09 +0900

    typescript Koa 초기설정

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Context } from 'koa';
+
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx: Context) => {
+  ctx.body = '2022년 5월 19일';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
