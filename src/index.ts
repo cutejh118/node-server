14b61de614c34993999adb4964f37b5bce77d1a7
cutejh118 <cooljh118@gmail.com>
2022-05-19 16:58:40 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { Context } from 'koa';
+import { Context } from "koa";
 
-const Koa = require('koa');
+const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx: Context) => {
-  ctx.body = '2022년 5월 19일';
+router.get("/", (ctx: Context, next: any) => {
+  ctx.body = "홈";
 });
 
+// app.use((ctx: Context) => {
+//   ctx.body = '2022년 5월 19일';
+// });
 
 app.listen(4000, () => {
-  console.log('Listening to port 4000');
+  console.log("Listening to port 4000");
 });
