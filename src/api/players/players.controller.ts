ddab6be6eea85db210de3873de1d4d99167bfa64
cutejh118 <cooljh118@gmail.com>
2022-05-24 12:54:26 +0900

    feat: Player 컨트롤러 함수 완성

--- src/api/players/players.controller.ts
+++ src/api/players/players.controller.ts
@@ -2,8 +2,16 @@ import { Context } from "koa";
 import Player from "../../entity/player.entity";
 import appDataSource from "../../connection/connect";
 
-const playerList = (ctx: Context) => {
-  ctx.body = "list 테스트";
+const playerList = async (ctx: Context) => {
+  try {
+    ctx.body = await appDataSource
+      .getRepository(Player)
+      .createQueryBuilder()
+      .execute();
+  } catch (error) {
+    console.dir(error);
+    ctx.throw(500);
+  }
 };
 
 const playerCreate = async (ctx: Context) => {
@@ -19,29 +27,74 @@ const playerCreate = async (ctx: Context) => {
           height: height,
           weight: weight,
           backNumber: backNumber,
-          birthday: birthday
+          birthday: birthday,
         },
       ])
       .execute();
   } catch (error) {
-      console.dir(error);
+    console.dir(error);
     ctx.throw(500);
   }
 };
-const playerRead = (ctx: Context) => {
+const playerRead = async (ctx: Context) => {
+  const { name } = ctx.params;
+  try {
+    ctx.body = await appDataSource
+      .createQueryBuilder()
+      .select("*")
+      .from(Player, "player")
+      .where("playerName = :name", { name })
+      .execute();
+  } catch (error) {
+    console.dir(error);
+    ctx.throw(500);
+  }
+  //선수명 입력
+};
+
+const playerDelete = async (ctx: Context) => {
+  const { id } = ctx.request.query;
+  const { name } = ctx.params;
+  try {
+    await appDataSource
+    .createQueryBuilder()
+    .delete()
+    .from(Player)
+    .where("playerName = :name  and id = :id "  ,  { name , id})
+    .execute();  
+  } catch (error) {
+    console.dir(error);
+    ctx.throw(500);   
+  } 
+   //지정선수 삭제
+};
+
+const playerUpdate = async (ctx: Context) => {
     const { id } = ctx.params;
-    ctx.body = id+'입니다.';
+    const { playerName, height, weight, backNumber, birthday } = ctx.request.body;
+  try {
+    await appDataSource
+    .createQueryBuilder()
+    .update(Player)
+    .set({
+        playerName: playerName,
+        height: height,
+        weight: weight,
+        backNumber: backNumber,
+        birthday: birthday
+    })
+    .where("id = :id", {id})
+    .execute()  
+  } catch (error) {
+    console.dir(error)
+    ctx.throw(500)
+  }
 };
 
-const playerDelete = (ctx: Context) => {
+export {
+  playerList,
+  playerCreate,
+  playerRead,
+  playerDelete,
+  playerUpdate,
 };
-
-const playerReplace = (ctx: Context) => {
-  ctx.body = "replace테스트";
-};
-
-const playerUpdate = (ctx: Context) => {
-  ctx.body = "update테스트";
-};
-
-export { playerList, playerCreate, playerRead, playerDelete, playerReplace, playerUpdate };
